a4235edd920cfaec3a1dcbbb1f4c3b4f8f37da24
Huang Wei <huang@humentsoft.com>
2012-05-04 11:29:09 +0800

    add libraries

---
+++ lib_old/vpopmail.rb
@@ -0,0 +1,16 @@
+# vpopmail: a wrapper for vpopmail management commandline
+#
+# @author Huang Wei <huang@huentsoft.com>
+# @date 2012/05/03
+
+## Vpopmail is a wrapper module for vpopmail command line tools.
+module Vpopmail
+  # define the directory holds all vpopmail commmand executables
+  HOME_DIR = "/home/vpopmail/"
+  DOMAIN_DIR = HOME_DIR + "domains/"
+
+  ## get domain, mail address and mailing list numbers
+  def summary
+
+  end
+end
---
+++ lib_old/vpopmail/domain.rb
@@ -0,0 +1,25 @@
+
+module Vpopmail
+
+  ## domain management tools
+  class Domain
+
+    ## add a new domain `domain_name`
+    # return boolean
+    def self.add(domain_name)
+
+    end
+
+    ## delete the domain `domain_name`
+    # return boolean
+    def self.del(domain_name)
+
+    end
+
+    ## get all domain list in a array of hash
+    # each hash contains :domain, :users (user number)
+    def self.list
+
+    end
+  end
+end
