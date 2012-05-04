adb23f9c68aa3dfad52a71c06b560b78aec6ab6e
Huang Wei <huang@humentsoft.com>
2012-05-04 15:47:08 +0800

    new load test for summary.

--- load.rb
+++ load.rb
@@ -3,4 +3,6 @@ require_relative "lib/vpopmail"
 vp = Vpopmail.new()
 
 #info = vp.dominfo
-p vp.get_ml(vp.dhash("aushop.gr.jp"))
+#p vp.get_ml(vp.dhash("aushop.gr.jp"))
+p vp.get_trans(vp.dhash("aushop.gr.jp"))
+p vp.summary()
